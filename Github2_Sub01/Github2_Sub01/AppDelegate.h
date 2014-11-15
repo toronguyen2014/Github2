b691087b002d780237b05a8e470733f85148e0f1
Thien NH <toronguyen2014@gmail.com>
2014-11-15 18:17:53 +0700

    Init

---
+++ Github2_Sub01/Github2_Sub01/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Github2_Sub01
+//
+//  Created by Toro on 11/15/14.
+//  Copyright (c) 2014 Toro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Github2_Sub01/Github2_Sub01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github2_Sub01
+//
+//  Created by Toro on 11/15/14.
+//  Copyright (c) 2014 Toro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
